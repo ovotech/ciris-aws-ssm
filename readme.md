1507a5c045d76a6e845f11b61d174da326134ec6
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2019-11-07 10:12:58 +0000

    Change to use blocker, move example to readme

--- readme.md
+++ readme.md
@@ -1,12 +1,73 @@
-# ciris-aws-ssm
+## Ciris AWS SSM
 
-[Amazon EC2 Systems Manager](https://aws.amazon.com/ec2/systems-manager/?nc2=h_m1) (a.k.a. Simple Systems Manager or SSM) integration for [Ciris](https://cir.is).
+[Amazon EC2 Systems Manager](https://aws.amazon.com/ec2/systems-manager/?nc2=h_m1) (also known as Simple Systems Manager or SSM) support for [Ciris](https://cir.is).
 
-This integration allows you to load parameters of type "string" or "secret string" from the SSM parameter store.
+Enables us to load parameters of type "string" or "secret string" from the SSM parameter store.
 
 See [this blog](https://medium.com/@mda590/simple-secrets-management-via-aws-ec2-parameter-store-737477e19450) for more information about using SSM's parameter store to manage secrets.
 
-## How to release
+### Getting Started
+
+To get started with [sbt](https://www.scala-sbt.org), simply add the following lines to your `build.sbt` file.
+
+```scala
+resolvers += Resolver.bintrayRepo("ovotech", "maven")
+
+libraryDependencies += "com.ovoenergy" %% "ciris-aws-ssm" % "1.0.0"
+```
+
+The library is published for Scala 2.12 and 2.13.
+
+### Example
+
+```scala
+import cats.effect.{Blocker, ExitCode, IO, IOApp}
+import cats.implicits._
+import ciris._
+import ciris.aws.ssm._
+import com.amazonaws.auth._
+import com.amazonaws.regions.Regions
+
+// the region can be overridden using an implicit
+implicit val region: Regions =
+  Regions.EU_WEST_1
+
+// the credentials provider can be overridden using an implicit
+implicit val credsProvider: AWSCredentialsProvider =
+  new DefaultAWSCredentialsProviderChain
+
+final case class Config(
+  username: String,
+  password: String,
+  port: Int,
+  alwaysNone: Option[String]
+)
+
+object Main extends IOApp {
+  def run(args: List[String]): IO[ExitCode] =
+    Blocker[IO].use { blocker =>
+      val config =
+        (
+          param("password", blocker),
+          param("port", blocker).as[Int],
+          param("random-entry", blocker).option
+        ).parMapN { (password, port, alwaysNone) =>
+          Config(
+            username = "Dave",
+            password = password,
+            port = port,
+            alwaysNone = alwaysNone
+          )
+        }
+
+      config.load[IO]
+    }.as(ExitCode.Success)
+}
+```
+
+### Release
+
+To release a new version, use the following command.
 
 ```
 $ sbt release
--- src/main/scala/ciris/aws/ssm/package.scala
+++ src/main/scala/ciris/aws/ssm/package.scala
@@ -1,5 +1,6 @@
 package ciris.aws
 
+import cats.effect.Blocker
 import ciris._
 import com.amazonaws.auth._
 import com.amazonaws.regions.Regions
@@ -7,41 +8,43 @@ import com.amazonaws.services.simplesystemsmanagement._
 import com.amazonaws.services.simplesystemsmanagement.model._
 
 package object ssm {
-  final def param(key: String)(
+  final def param(key: String, blocker: Blocker)(
     implicit region: Regions = Regions.EU_WEST_1,
     credsProvider: AWSCredentialsProvider = new DefaultAWSCredentialsProviderChain()
   ): ConfigValue[String] =
-    ConfigValue.suspend {
-      val ssmClient =
-        AWSSimpleSystemsManagementClientBuilder
-          .standard()
-          .withRegion(region)
-          .withCredentials(credsProvider)
-          .build()
+    ConfigValue.blockOn(blocker) {
+      ConfigValue.suspend {
+        val ssmClient =
+          AWSSimpleSystemsManagementClientBuilder
+            .standard()
+            .withRegion(region)
+            .withCredentials(credsProvider)
+            .build()
 
-      val configKey =
-        ConfigKey(s"parameter $key from AWS SSM")
+        val configKey =
+          ConfigKey(s"parameter $key from AWS SSM")
 
-      try {
-        val result =
-          ssmClient.getParameter {
-            new GetParameterRequest()
-              .withWithDecryption(true)
-              .withName(key)
-          }
+        try {
+          val result =
+            ssmClient.getParameter {
+              new GetParameterRequest()
+                .withWithDecryption(true)
+                .withName(key)
+            }
 
-        val loaded =
-          for {
-            parameter <- Option(result.getParameter)
-            value <- Option(parameter.getValue)
-          } yield ConfigValue.loaded(configKey, value)
+          val loaded =
+            for {
+              parameter <- Option(result.getParameter)
+              value <- Option(parameter.getValue)
+            } yield ConfigValue.loaded(configKey, value)
 
-        loaded.getOrElse(ConfigValue.missing(configKey))
-      } catch {
-        case _: ParameterNotFoundException =>
-          ConfigValue.missing(configKey)
-      } finally {
-        ssmClient.shutdown()
+          loaded.getOrElse(ConfigValue.missing(configKey))
+        } catch {
+          case _: ParameterNotFoundException =>
+            ConfigValue.missing(configKey)
+        } finally {
+          ssmClient.shutdown()
+        }
       }
     }
 }
